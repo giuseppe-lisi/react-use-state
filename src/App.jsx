fc1f8574ed0a368da46de0ee70b8551407956ca9
Giuseppe Lisi <giuseppe.lisi.main@gmail.com>
2026-01-17 16:38:29 +0100

    vengono stampati a schermo i bottoni tramite .map e viene assegnata ora una key per ognuno. Iniziato a lavorare al funzionamento della card

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,21 @@ import { useState } from "react";
 import languages from "./assets/data/languages";
 
 function App() {
+    
     return (
         <>
-            <button>bottone 1</button>
+            <h1>Learn Web Development</h1>
+            <ul className="flex">
+                {/* stampa in pagina i bottoni */}
+                {languages.map((language, i)=>{
+                    return <li key={i}><button>{language.title}</button></li>
+                })}
+            </ul>
+
+            <div className="card">
+                <h2>{languages[0].title}</h2>
+                <p>{languages[0].description}</p>
+            </div>
         </>
     )
 }
